11db11898324034ca915dc0cbd7c117fb93b15ad
rr- <rr-@sakuya.pl>
2017-09-30 16:51:18 +0200

    refactor

--- 10ff/__main__.py
+++ 10ff/__main__.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-import re
-import sys
-import time
-import random
-import shutil
+import asyncio
 import argparse
 import pathlib
-import asyncio
+import re
+import time
+import random
+import sys
+import shutil
 import tty
 import termios
 
@@ -53,8 +53,33 @@ class RawTerminal:
     def __init__(self, loop):
         self._fd = sys.stdin.fileno()
         self._loop = loop
+        self._old_settings = None
         self.input_queue = asyncio.Queue(loop=loop)
         loop.add_reader(sys.stdin, self._got_input)
+
+    @staticmethod
+    def move_cursor_up(num):
+        sys.stdout.write(f'\x1B[{num}F')
+
+    @staticmethod
+    def set_red_font():
+        sys.stdout.write('\x1B[31;1m')
+
+    @staticmethod
+    def set_green_font():
+        sys.stdout.write('\x1B[32;1m')
+
+    @staticmethod
+    def set_yellow_font():
+        sys.stdout.write('\x1B[33;1m')
+
+    @staticmethod
+    def set_default_font():
+        sys.stdout.write('\x1B[39m')
+
+    @staticmethod
+    def erase_whole_line():
+        sys.stdout.write('\x1B[999D\x1B[K')
 
     def enable(self):
         self._old_settings = termios.tcgetattr(self._fd)
@@ -70,142 +95,140 @@ class RawTerminal:
 
 class GameState:
     def __init__(self, words):
-        self.words = words
-        self.current_word = 0
-        self.text_input = ''
-        self.status = [STATUS_UNTYPED for _ in words]
-        self.status[self.current_word] = STATUS_TYPING
-        self.line_boundaries = divide_lines(words)
-        self.time_left = MAX_TIME
-        self.start_time = time.time()
-        self.end_time = None
-        self.total_keys_pressed = 0
-        self.current_word_keys_pressed = 0
-        self._first_run = True
-
-    @property
-    def current_line(self):
-        for i, (low, high) in enumerate(self.line_boundaries):
-            if self.current_word in range(low, high):
-                return i
-        return len(self.line_boundaries) - 1
-
-    @property
-    def shown_line_boundaries(self):
-        current = self.current_line
-        for i in range(MAX_LINES):
-            if current + i in range(len(self.line_boundaries)):
-                yield self.line_boundaries[current + i]
+        self._words = words
+        self._current_word = 0
+        self._text_input = ''
+        self._status = [STATUS_TYPING] + [STATUS_UNTYPED for _ in words[1:]]
+        self._line_boundaries = divide_lines(words)
+        self._time_left = MAX_TIME
+        self._start_time = time.time()
+        self._end_time = None
+        self._total_keys_pressed = 0
+        self._current_word_keys_pressed = 0
+        self._first_render = True
 
     @property
     def finished(self):
-        return self.end_time is not None
+        return self._end_time is not None
+
+    @property
+    def _current_line(self):
+        for i, (low, high) in enumerate(self._line_boundaries):
+            if self._current_word in range(low, high):
+                return i
+        return len(self._line_boundaries) - 1
+
+    @property
+    def _shown_line_boundaries(self):
+        current = self._current_line
+        for i in range(MAX_LINES):
+            if current + i in range(len(self._line_boundaries)):
+                yield self._line_boundaries[current + i]
 
     def render(self):
-        shown_line_boundaries = list(self.shown_line_boundaries)
-        if not self._first_run:
-            sys.stdout.write(f'\x1B[{MAX_LINES + 1}F')
-        self._first_run = False
+        shown_line_boundaries = list(self._shown_line_boundaries)
+        if not self._first_render:
+            RawTerminal.move_cursor_up(MAX_LINES + 1)
+        self._first_render = False
 
         for i in range(MAX_LINES):
-            sys.stdout.write('\x1B[K')
+            RawTerminal.erase_whole_line()
             if i in range(len(shown_line_boundaries)):
                 low, high = shown_line_boundaries[i]
                 for idx in range(low, high):
-                    if self.status[idx] == STATUS_TYPING:
-                        sys.stdout.write('\x1B[33;1m')
-                    elif self.status[idx] == STATUS_TYPED_WELL:
-                        sys.stdout.write('\x1B[32;1m')
-                    elif self.status[idx] == STATUS_TYPED_WRONG:
-                        sys.stdout.write('\x1B[31;1m')
+                    if self._status[idx] == STATUS_TYPING:
+                        RawTerminal.set_yellow_font()
+                    elif self._status[idx] == STATUS_TYPED_WELL:
+                        RawTerminal.set_green_font()
+                    elif self._status[idx] == STATUS_TYPED_WRONG:
+                        RawTerminal.set_red_font()
                     else:
-                        sys.stdout.write('\x1B[39m')
-                    print(self.words[idx], end='')
-                    sys.stdout.write('\x1B[39m')
+                        RawTerminal.set_default_font()
+                    print(self._words[idx], end='')
                     print(end=' ')
             print()
-        sys.stdout.write('\x1B[K')
-        print(f'--- ({self.time_left} s left) ---')
-        sys.stdout.write('\x1B[K')
-        print(self.text_input, end='', flush=True)
+        RawTerminal.erase_whole_line()
+        print(f'--- ({self._time_left} s left) ---')
+        RawTerminal.erase_whole_line()
+        print(self._text_input, end='', flush=True)
 
     def render_stats(self):
         correct_words = [
             word
-            for word, status in zip(self.words, self.status)
+            for word, status in zip(self._words, self._status)
             if status == STATUS_TYPED_WELL]
         wrong_words = [
             word
-            for word, status in zip(self.words, self.status)
+            for word, status in zip(self._words, self._status)
             if status == STATUS_TYPED_WRONG]
         correct_keystrokes = sum(len(word) + 1 for word in correct_words)
         wrong_keystrokes = sum(len(word) + 1 for word in wrong_words)
         total_keystrokes = correct_keystrokes + wrong_keystrokes
 
-        cps = correct_keystrokes / (self.end_time - self.start_time)
+        cps = correct_keystrokes / (self._end_time - self._start_time)
         wpm = cps * 60.0 / WORD_LENGTH
         accurracy = (
-            correct_keystrokes / self.total_keys_pressed
-            if self.total_keys_pressed else 1)
+            correct_keystrokes / self._total_keys_pressed
+            if self._total_keys_pressed else 1)
 
-        sys.stdout.write('\x1B[999D\x1B[K')
+        RawTerminal.erase_whole_line()
 
         print(f'CPS (chars per second): {cps:.1f}')
         print(f'WPM (words per minute): {wpm:.1f}')
 
         print(f'Keys pressed:           {total_keystrokes} (', end='')
-        sys.stdout.write('\x1B[32;1m')
+        RawTerminal.set_green_font()
         print(correct_keystrokes, end='|')
-        sys.stdout.write('\x1B[31;1m')
+        RawTerminal.set_red_font()
         print(wrong_keystrokes, end='')
-        sys.stdout.write('\x1B[39m')
+        RawTerminal.set_default_font()
         print(')')
 
-        print(f'Total keys pressed:     {self.total_keys_pressed}')
+        print(f'Total keys pressed:     {self._total_keys_pressed}')
         print(f'Accurracy:              {accurracy:.1%}')
 
         print(r'Correct words:          ', end='')
-        sys.stdout.write('\x1B[32;1m')
+        RawTerminal.set_green_font()
         print(len(correct_words), end='')
-        sys.stdout.write('\x1B[39m')
+        RawTerminal.set_default_font()
         print()
 
         print(r'Wrong words:            ', end='')
-        sys.stdout.write('\x1B[31;1m')
+        RawTerminal.set_red_font()
         print(len(wrong_words), end='')
-        sys.stdout.write('\x1B[39m')
+        RawTerminal.set_default_font()
         print()
 
     def backspace_pressed(self):
-        self.text_input = self.text_input[:-1]
-        self.current_word_keys_pressed += 1
+        self._text_input = self._text_input[:-1]
+        self._current_word_keys_pressed += 1
 
     def key_pressed(self, key):
-        self.text_input += key
-        self.current_word_keys_pressed += 1
+        self._text_input += key
+        self._current_word_keys_pressed += 1
 
     def word_finished(self):
-        self.total_keys_pressed += self.current_word_keys_pressed + 1
-        self.current_word_keys_pressed = 0
-        self.status[self.current_word] = (
+        self._total_keys_pressed += self._current_word_keys_pressed + 1
+        self._current_word_keys_pressed = 0
+        self._status[self._current_word] = (
             STATUS_TYPED_WELL
-            if self.words[self.current_word] == self.text_input
+            if self._words[self._current_word] == self._text_input
             else STATUS_TYPED_WRONG)
-        self.text_input = ''
+        self._text_input = ''
 
-        self.current_word += 1
-        if self.current_word == len(self.words):
+        self._current_word += 1
+        if self._current_word == len(self._words):
             self.finish()
             return
 
-        self.status[self.current_word] = STATUS_TYPING
+        self._status[self._current_word] = STATUS_TYPING
 
     def finish(self):
-        self.end_time = time.time()
+        self._end_time = time.time()
 
     def tick(self):
-        self.time_left -= 1
-        if self.time_left == 0:
+        self._time_left -= 1
+        if self._time_left == 0:
             self.finish()
 
 
