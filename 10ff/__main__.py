e77255e04204ac69348f26f5de33807de07d3f13
rr- <rr-@sakuya.pl>
2019-12-24 09:53:57 +0100

    improve help

--- 10ff/__main__.py
+++ 10ff/__main__.py
@@ -5,13 +5,47 @@ from .game import Game
 from .util import get_corpus_path
 
 DEFAULT_TIME = 60
+PROLOG = (
+    "A certain typing contest site spin-off in CLI, without all the "
+    "advertisements, tracking and 10 megabytes of AJAX crap."
+)
+
+
+class CustomHelpFormatter(argparse.HelpFormatter):
+    def _format_action_invocation(self, action):
+        if not action.option_strings or action.nargs == 0:
+            return super()._format_action_invocation(action)
+        default = self._get_default_metavar_for_optional(action)
+        args_string = self._format_args(action, default)
+        return ", ".join(action.option_strings) + " " + args_string
 
 
 def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--time", type=int, default=DEFAULT_TIME)
-    parser.add_argument("-c", "--corpus", type=str, default="english")
-    parser.add_argument("-w", "--width", type=int, default=80)
+    fmt = lambda prog: CustomHelpFormatter(prog)
+    parser = argparse.ArgumentParser(
+        prog="10ff", description=PROLOG, formatter_class=fmt
+    )
+    parser.add_argument(
+        "-t",
+        "--time",
+        type=int,
+        default=DEFAULT_TIME,
+        help="how long to play the game for (in seconds)",
+    )
+    parser.add_argument(
+        "-c",
+        "--corpus",
+        type=str,
+        default="english",
+        help="path to the word list to play the game with",
+    )
+    parser.add_argument(
+        "-w",
+        "--width",
+        type=int,
+        default=80,
+        help="with of the terminal to play in",
+    )
     return parser.parse_args()
 
 
