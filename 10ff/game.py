ced90498b6166b3987b38339fbfe5526f86137cf
rr- <rr-@sakuya.pl>
2019-12-24 11:45:36 +0100

    don't commit special keys as text

--- 10ff/game.py
+++ 10ff/game.py
@@ -240,7 +240,7 @@ class Game:
             elif re.match(r"\s", key):
                 if state._text_input != "" or self._rigorous_spaces:
                     state.word_finished()
-            else:
+            elif ord(key) >= 32:
                 state.key_pressed(key)
 
         await timer_future
